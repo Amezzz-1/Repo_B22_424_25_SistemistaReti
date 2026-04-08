6091659a679e7f536722841094d7d75d56e571a0
= <=>
2026-04-08 17:44:09 +0200

    up

---
+++ Lezioni_Python/Lezione2_Statement/esempi_IF.py
@@ -0,0 +1,7 @@
+#Es1. Puoi accedere al club se sei maggiorenne e hai un doc valido
+
+#Es2. Username e password. Chiedi all'utente entrambi se sono user == "admin" e pass == "1234" l'utente può accedere
+
+#Es3. Dato un certo numero verifica se si trova tra 20 e 100 oppure se si trova tra 50 e 200
+
+#Es4. Gioco dei dadi. Lancia 2 dadi (numero casuale tra 1 e un numero scelto dall'utente. Faccio scegliere all'utente il numero di facce del dado). Se i due numeri sono uguali l'utente vince, altrimenti perde.
