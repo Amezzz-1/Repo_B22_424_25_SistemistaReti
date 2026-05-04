c7b12a572a6f45c6957f1ee740354185105d48e7
Dario <dario.mennillo@immaginazioneelavoro.it>
2026-05-04 19:48:41 +0200

    Create 03_esempio.py

---
+++ Lezioni_Python/Lezione4_Dizionari/03_esempio.py
@@ -0,0 +1,13 @@
+# Crea un dizionare "auto" con le seguenti chiavi:
+# marca, modello, anno, km.
+# Stampa tutte le coppie chiave-valore di tutti i dizionari
+
+############################
+# Data la seguente lista di dizionari, stampa solo i prodotti disponibili con nome e prezzo
+
+prodotti = [
+    {"nome": "mouse", "prezzo": 19.99, "quantita": 100, "disponibilità": True},
+    {"nome": "tastiera", "prezzo": 25.49, "quantita": 50, "disponibilità": True},
+    {"nome": "monitor", "prezzo": 105.50, "quantita": 8, "disponibilità": True},
+    {"nome": "webcam", "prezzo": 9.99, "quantita": 0, "disponibilità": False},
+]
