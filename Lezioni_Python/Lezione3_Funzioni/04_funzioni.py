fba270e7d9cd38d902ad7c2276a249739b04a8d2
Dario <dario.mennillo@immaginazioneelavoro.it>
2026-05-04 16:17:28 +0200

    Create 04_funzioni.py

---
+++ Lezioni_Python/Lezione3_Funzioni/04_funzioni.py
@@ -0,0 +1,81 @@
+import random
+
+
+# Dichiaro la funzione
+def benvenuto():
+    print("Ciao Dario")
+    numeroCasuale = random.randint(1,100)
+    print(f"Oggi il tuo numero fortunato è il {numeroCasuale}")
+
+
+# eseguo la funzione
+benvenuto()
+
+# serverAttivi = []
+
+# # Le funzioni possono avere dei parametri in ingresso
+# def descrivi_server(nome, ip):
+#     if(ip == "192.168.1.25"):
+#         print("Attenzione, questo server non esiste più")
+#         return False
+#     else:
+#         print(f"Server: {nome} - IP: {ip}")
+#         return True
+
+# # descrivi_server("WebServer", "192.168.1.20")
+# # descrivi_server("DBServer", "192.168.1.10")
+# # descrivi_server("BEServer", "192.168.1.25")
+
+
+# def accendi_server():
+#     while len(serverAttivi) <= 3:
+#         ip_server = input("Inserisci ip del server: \n")
+#         nome_server = input("Inserisci il nome del server: \n")    
+#         if descrivi_server(nome_server, ip_server):
+#             server = ip_server + " - " + nome_server
+#             serverAttivi.append(server)
+
+
+# accendi_server()
+
+
+# Funzioni con return
+
+def scrivi_nome(nome):
+    descrizione = "Il tuo nome è " + nome
+    return descrizione #Questa funzione sta restituendo una stringa
+
+# Se ho una funzione con return sono obbligato a raccogliere quel valore
+print(scrivi_nome("Anna"))
+
+def stampa_numero():
+    numero_casuale = random.randint(1,20)
+    return numero_casuale
+
+def controlla_numero():
+    numero_da_controllare = stampa_numero() #raccolgo in una variabile il risultato di una funzione
+
+    if numero_da_controllare >= 10:
+        print(f"Ti trovi sopra il 10, il numero è {numero_da_controllare}")
+    else:
+        print(f"Ti trovi sotto il 10, il numero è {numero_da_controllare}")
+
+controlla_numero()
+
+
+
+def lancia_moneta():
+    testa_croce = random.randint(1,100)
+    print(f"Il numero di controllo è: {testa_croce}")
+    if testa_croce % 2 == 0 :
+        return True
+    else:
+        return False
+
+def controlla_moneta():
+    if lancia_moneta():
+        print("E' uscito testa")
+    else:
+        print("E' uscito croce")
+
+controlla_moneta()
